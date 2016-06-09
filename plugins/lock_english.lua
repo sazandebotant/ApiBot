94f81a0e95094557dfb472cca754d7be07fa2fab
sazandebotant <kiarash1@kiava.ir>
2016-06-09 17:49:17 +0430

    Update lock_english.lua

--- plugins/lock_english.lua
+++ plugins/lock_english.lua
@@ -9,7 +9,7 @@ local name = user_print_name(msg.from)
     local user = "user#id"..msg.from.id
 if msg.to.type == 'channel' and not is_momod(msg) then
  delete_msg(msg.id, ok_cb, true)
-send_large_msg(chat, "👤کاربر : [ @" .. msg.from.username .. "] \n💠شناسه کاربر : [" .. msg.from.id .."] \n ❌در حال فرستادن اموجی است\n 🚷پیام پاک شد" )
+send_large_msg(chat, "👤کاربر : [ @" .. msg.from.username .. "] \n💠شناسه کاربر : [" .. msg.from.id .."] \n ❌در حال فرستادن کلمه خارجی است\n 🚷پیام پاک شد" )
       end
    end
 end
