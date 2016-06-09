3b5a758045501c0dc9ba43924a80f52d5e1f33e5
sazandebotant <kiarash1@kiava.ir>
2016-06-09 13:00:36 +0430

    Update lock_ads.lua

--- plugins/lock_ads.lua
+++ plugins/lock_ads.lua
@@ -1,20 +1,23 @@
-local function run(msg)
-    
+local function run(msg, matches)
+    if is_momod(msg) then
+        return
+    end
     local data = load_data(_config.moderation.data)
-    
-     if data[tostring(msg.to.id)]['settings']['ads'] == 'yes' then
-      
+    if data[tostring(msg.to.id)] then
+        if data[tostring(msg.to.id)]['settings'] then
+            if data[tostring(msg.to.id)]['settings']['ads'] then
+                ads = data[tostring(msg.to.id)]['settings']['ads']
+            end
+        end
+    end
     local chat = get_receiver(msg)
-local name = user_print_name(msg.from)
     local user = "user#id"..msg.from.id
-if msg.to.type == 'channel' and not is_momod(msg) then
-      delete_msg(msg.id, ok_cb, true)
-send_large_msg(chat, "👤کاربر :  [ @" .. msg.from.username .. "] \n💠شناسه کاربر :  [" .. msg.from.id .."] \n ❌در حال فرستادن لینک است\n 🚷پیام پاک شد" ) 
-      end
-   end
+    if ads == "yes" then
+        delete_msg(msg.id, ok_cb, true)
+       send_large_msg(chat, "👤کاربر :   [ @" ..msg.from.username .. "] \n💠شناسه کاربر : [" ..   msg.from.id .."] \n ❌در حال فرستادن لینک است\n 🚷پیام پاک شد" ) 
+    end
 end
-
-    
+ 
 return {patterns = {
 "[Hh]ttps://(.*)",
 "[Hh][Tt][Tt][Pp][Ss]://[Tt][Ee][Ll][Ee][Gg][Rr][Aa][Mm].[Mm][Ee]/(.*)",
@@ -112,6 +115,3 @@ return {patterns = {
   },
   run = run
 }
---coded By @kiava
---designed by @mohammad_nbg
---Copy Right For kiava.ir 2015-2016
