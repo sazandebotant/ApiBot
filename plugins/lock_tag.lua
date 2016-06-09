50f5c079180438ecdb8a0e60060e317f78f0f7be
sazandebotant <kiarash1@kiava.ir>
2016-06-09 13:45:25 +0430

    Update lock_tag.lua

--- plugins/lock_tag.lua
+++ plugins/lock_tag.lua
@@ -1,27 +1,30 @@
-local function run(msg)
-    
+local function run(msg, matches)
+    if is_owner(msg) then
+        return
+    end
     local data = load_data(_config.moderation.data)
-    
-     if data[tostring(msg.to.id)]['settings']['tag'] == 'yes' then
-      
+    if data[tostring(msg.to.id)] then
+        if data[tostring(msg.to.id)]['settings'] then
+            if data[tostring(msg.to.id)]['settings']['tag'] then
+                tag = data[tostring(msg.to.id)]['settings']['tag']
+            end
+        end
+    end
     local chat = get_receiver(msg)
-local name = user_print_name(msg.from)
     local user = "user#id"..msg.from.id
-if msg.to.type == 'channel' and not is_momod(msg) then
-  kick_user(msg.from.id, msg.to.id)
- delete_msg(msg.id, ok_cb, true)
-send_large_msg(chat, "👤کاربر :  [ @" .. msg.from.username .. "] \n💠شناسه کاربر :  [" .. msg.from.id .."] \n 👁‍🗨دلیل : ارسال هشتگ در گروه\n 🚷اخراج شد  " )
-  
-      end
-   end
+    if tag == "yes" then
+        delete_msg(msg.id, ok_cb, true)
+    end
 end
-    
+ 
 return {
-patterns = {
-  "#",
-  "#(.*)"
+  patterns = {
+    "#(.*)",
+    "# (.*)",
+    "(.*)#",
+    "(.*) #",
+    "#"
   },
   run = run
 }
-
 
