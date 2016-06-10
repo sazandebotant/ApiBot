bd043c7b17a09d9d69cac6ec6887fa21dd44169b
sazandebotant <kiarash1@kiava.ir>
2016-06-10 15:30:00 +0430

    Add files via upload

--- plugins/v-card.lua
+++ plugins/v-card.lua
@@ -3,6 +3,7 @@ do
 local function run(msg, matches)
     local Mohammad = 147237496
     local Kiava = 174997972
+    local pooya = 129622269
       if msg.text:find('cart') then
     if msg.from.id == tonumber(Mohammad) then
         local text = [[
@@ -14,7 +15,7 @@ local function run(msg, matches)
 🔖Phone number: +98939830**** 
 ⭐️ Team name: kiavaprotection
 📊 Level: 2
-📝Status: Editor & Developer
+📝Status: Editor & thinker
 
         ]]
  send_document(get_receiver(msg), "/root/parniangh123/data/sticker/147237496.webp", ok_cb, false)
@@ -34,7 +35,23 @@ local function run(msg, matches)
  📝Status: Programmer & Manager
         ]]
    send_document(get_receiver(msg), "/root/parniangh123/data/sticker/174997972.webp", ok_cb, false)
+
         return text
+
+else  if msg.from.id == tonumber(pooya) then
+
+     local text = [[
+🔖Name : pooya
+🔖 Last name: ——
+🔖Age: 15
+🔖 Username: @pooya8351
+🔖Phone number: +989147438340 
+⭐️ Team name: kiavaprotection
+📊 Level: 1
+📝Status: Webmaster & graphist & little coder
+       ]]
+ send_document(get_receiver(msg), "/root/parniangh123/data/sticker/129622269.webp", ok_cb, false)
+return text
     else  if not is_sudo(msg) then
           return "🔖شما جزء تیم کیاوا نیستید یا کارت شناسایی برای شما صادر نشده است"
 
