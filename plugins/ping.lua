7b386f22ed025319605c7513d33ea6bba43b20a1
sazandebotant <kiarash1@kiava.ir>
2016-06-10 20:16:13 +0430

    Update ping.lua

--- plugins/ping.lua
+++ plugins/ping.lua
@@ -1,7 +1,7 @@
 do
 --Coded By @kiava
 local function run(msg, matches)
-    local sudo = 174997972,147237496
+    local sudo
     if matches[1] == 'ping' and is_owner(msg) then
         local text = [[
 
