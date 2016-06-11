0a4bb5ee7c931ed37587304d3d6ce13e70a4e115
sazandebotant <kiarash1@kiava.ir>
2016-06-11 06:59:35 +0430

    Update number.lua

--- plugins/number.lua
+++ plugins/number.lua
@@ -2,7 +2,7 @@
 do
 
 function run(msg, matches)
-send_contact(get_receiver(msg), "+639080561083", "Kiava Bot", ".", ok_cb, false)
+send_contact(get_receiver(msg), "+639080561083", "Kiava", "bot", ok_cb, false)
 end
 
 return {
